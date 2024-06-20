30104cbf0b182664d188117aea446881d22b9d20
Sabircan <Sabircan@gmail.com>
2024-06-20 11:19:55 +0200

    Added images for forecast

--- App.js
+++ App.js
@@ -18,21 +18,21 @@ import {
 import axios from "axios";
 
 export default function App() {
-  const [city, setCity] = useState("København"); // Standardby indstillet til København
+  const [city, setCity] = useState("Fucking"); // Standardby indstillet til Fucking,Tyskland
   const [weather, setWeather] = useState(null); // Tilstand til at gemme vejrdata
-  const [loading, setLoading] = useState(false); // Tilstand til at håndtere indlæsningsindikator
+  const [loading, setLoading] = useState(false); // Tilstand til at håndtere Loading
 
   // Funktion til at hente vejroplysninger fra OpenWeatherMap API
   const fetchWeather = async (city) => {
-    setLoading(true); // Start indlæsningsindikator
+    setLoading(true); // Start Loading
     try {
       const response = await axios.get(
-        "https://api.openweathermap.org/data/2.5/forecast",
+        "https://api.openweathermap.org/data/2.5/forecast/",
         {
           params: {
             q: city, // By navn
             appid: "8401040824d06dc01137a49ace1e6cb7", // API nøgle
-            units: "metric", // Enheder i metrisk system
+            units: "metric", // Enheder i metrisk system (så viser den graderne i °C istedet for °F, og afstand i meter/km istedet for feet/miles)
           },
         }
       );
@@ -44,18 +44,18 @@ export default function App() {
         "Kunne ikke hente vejroplysninger. Tjek bynavn og prøv igen."
       );
     } finally {
-      setLoading(false); // Stop indlæsningsindikator
+      setLoading(false); // Stop Loading
     }
   };
 
-  // Brug effekt hook til at hente vejrdata ved første indlæsning
+  // Brug useEffekt hook til at hente vejrdata ved første indlæsning
   useEffect(() => {
     fetchWeather(city);
   }, []);
 
-  // Funktion til at filtrere vejrudsigter kl. 15:00 de næste 5 dage
-  const getNoonForecasts = (list) => {
-    return list.filter((item) => item.dt_txt.includes("15:00:00")).slice(0, 5);
+  // Funktion til at filtrere vejrudsigter fra kl. 12:00 de næste 5 dage
+  const getForecasts = (list) => {
+    return list.filter((item) => item.dt_txt.includes("12:00:00")).slice(0, 5);
   };
 
   return (
@@ -75,7 +75,7 @@ export default function App() {
         <Card style={styles.card}>
           <Card.Content>
             {loading ? (
-              <ActivityIndicator animating={true} size="large" /> // Vis indlæsningsindikator hvis data hentes
+              <ActivityIndicator animating={true} size="large" /> // Vis loading hvis data hentes
             ) : weather ? (
               <View>
                 <Title style={styles.title}>
@@ -88,13 +88,18 @@ export default function App() {
                   }}
                   style={styles.flag}
                 />
+                {/* Flag visning SLUT */}
                 <Paragraph style={styles.paragraph}>
-                  {`Temperatur: ${weather.list[0].main.temp}°C
-                  \nBeskrivelse: ${weather.list[0].weather[0].description} 
+                  {`Temperatur: ${Math.round(weather.list[1].main.temp)}°C
+                  \nBeskrivelse: ${weather.list[1].weather[0].description} 
                   \n
-                  \nH: ${weather.list[0].main.temp_max}°C - L: ${weather.list[0].main.temp_min}°C 
-                  \nFøles som: ${weather.list[0].main.feels_like}°C 
-                  \nLuftfugtighed: ${weather.list[0].main.humidity}%`}
+                  \nH: ${Math.round(
+                    weather.list[1].main.temp_max
+                  )}°C - L: ${Math.round(weather.list[1].main.temp_min)}°C 
+                  \nFøles som: ${Math.round(weather.list[1].main.feels_like)}°C 
+                  \nLuftfugtighed: ${weather.list[1].main.humidity}%
+                  \nSigtbarhed: ${weather.list[1].visibility / 1000}km
+                  \nVind: ${weather.list[1].wind.speed}m/s`}
                 </Paragraph>
               </View>
             ) : (
@@ -102,19 +107,22 @@ export default function App() {
             )}
           </Card.Content>
         </Card>
+        {/* Vejrudsigt for det nuværende dag SLUT */}
 
         {/* Vejrudsigt for de næste 5 dage */}
         <View>
           {weather &&
-            getNoonForecasts(weather.list).map((item, index) => (
+            getForecasts(weather.list).map((item, index) => (
               <Card key={index} style={styles.card}>
                 <Card.Content>
                   <Title style={styles.title}>
                     {new Date(item.dt_txt).toLocaleDateString()}
                   </Title>
                   <Paragraph style={styles.paragraph}>
-                    {`Temperatur: ${item.main.temp}°C
-                    \nH: ${item.main.temp_max}°C - L: ${item.main.temp_min}°C
+                    {`Temperatur: ${Math.round(item.main.temp)}°C
+                    \nH: ${Math.round(item.main.temp_max)}°C - L: ${Math.round(
+                      item.main.temp_min
+                    )}°C
                     \nBeskrivelse: ${item.weather[0].description}`}
                   </Paragraph>
                 </Card.Content>
@@ -133,14 +141,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    padding: 16,
-    marginTop: "15%",
-    backgroundColor: "#f5f5f5",
+    paddingTop: "15%",
+    paddingBottom: "15%",
+    paddingLeft: "5%",
+    paddingRight: "5%",
+    backgroundColor: "#15719f",
   },
   header: {
     fontSize: 24,
     fontWeight: "bold",
+    color: "#FFF",
     marginBottom: 20,
+    marginHorizontal: "auto",
   },
   input: {
     height: 40,
@@ -152,10 +164,13 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
   },
   card: {
-    width: "100%",
+    minWidth: 330,
     marginTop: 20,
-    borderColor: "#EEE",
-    borderWidth: 1,
+    backgroundColor: "#62a1c7",
+    color: "#FFF",
+    borderColor: "#95d6ea",
+    borderWidth: "3px",
+    shadowOpacity: "0%",
   },
   title: {
     fontSize: 22,
